392b5d4993f9729b6a28f3c97965b22eaceeff3e
luccanog <luccathomaz@gmail.com>
2023-09-25 16:53:33 -0300

    basic code wip

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+
+namespace Chat.gRPC.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public IServerStreamWriter<ServerMessage> StreamWriter { get; set; }
+
+        public User(string name, IServerStreamWriter<ServerMessage> streamWriter)
+        {
+            Name = name;
+            StreamWriter = streamWriter;
+        }
+    }
+}
--- Services/ChatServer.cs
+++ Services/ChatServer.cs
@@ -1,10 +1,13 @@
+using Chat.gRPC.Models;
 using Grpc.Core;
+using System.Collections.Concurrent;
 
 namespace Chat.gRPC.Services
 {
     public class ChatServer : ChatService.ChatServiceBase
     {
         private readonly ILogger<ChatServer> _logger;
+        private static readonly ConcurrentDictionary<string, List<User>> _chatRooms = new ConcurrentDictionary<string, List<User>>();
 
         public ChatServer(ILogger<ChatServer> logger)
         {
@@ -14,6 +17,90 @@ namespace Chat.gRPC.Services
         public override Task HandleCommunication(IAsyncStreamReader<ClientMessage> requestStream, IServerStreamWriter<ServerMessage> responseStream, ServerCallContext context)
         {
             return base.HandleCommunication(requestStream, responseStream, context);
+        }
+
+        public async Task<ClientMessage> ReadMessageWithTimeoutAsync(IAsyncStreamReader<ClientMessage> requestStream, TimeSpan timeout)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(timeout);
+
+            try
+            {
+                bool isMovingNext = await requestStream.MoveNext(cancellationTokenSource.Token);
+
+                if (!isMovingNext)
+                {
+                    throw new Exception("Connection dropped");
+                }
+
+                return requestStream.Current;
+            }
+            catch (TimeoutException)
+            {
+                _logger.LogError($"{nameof(ReadMessageWithTimeoutAsync)} - Timeout");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{nameof(ReadMessageWithTimeoutAsync)} - Error: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task AddClientToChatRoom(string chatRoomId, User user)
+        {
+            if (!_chatRooms.ContainsKey(chatRoomId))
+            {
+                _chatRooms[chatRoomId] = new List<User> { user };
+            }
+            else
+            {
+                var userAlreadyExist = _chatRooms[chatRoomId].Any(c => c.Name == user.Name);
+                if (userAlreadyExist)
+                {
+                    throw new InvalidOperationException("User with the same name already exists in the chat room");
+                }
+
+                _chatRooms[chatRoomId].Add(user);
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task StreamClientJoinedRoomServerMessage(string chatRoomId, string userName)
+        {
+            if (_chatRooms.ContainsKey(chatRoomId))
+            {
+                var serverMessage = new ServerMessage { UserJoined = new ServerMessageUserJoined { UserName = userName } };
+
+                var tasks = new List<Task>();
+
+                foreach (var user in _chatRooms[chatRoomId])
+                {
+                    if (user is not null)
+                    {
+                        tasks.Add(user.StreamWriter.WriteAsync(serverMessage));
+                    }
+                }
+
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        public async Task StreamServerMessage(string chatRoomId, string senderName, string message)
+        {
+            if (_chatRooms.ContainsKey(chatRoomId))
+            {
+                var tasks = new List<Task>();
+                var serverMessage = new ServerMessage { Chat = new ServerMessageChat { UserName = senderName, Text = message } };
+
+                foreach (var user in _chatRooms[chatRoomId])
+                {
+                    if (user is not null && user.Name != senderName)
+                    {
+                        tasks.Add(user.StreamWriter.WriteAsync(serverMessage));
+                    }
+                }
+                await Task.WhenAll(tasks);
+            }
         }
     }
 }
