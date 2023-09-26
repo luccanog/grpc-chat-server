b31524db3d562c93caad8c3a6b839d7616ab0fef
luccanog <luccathomaz@gmail.com>
2023-09-26 00:11:00 -0300

    adding interceptor tests

---
+++ src/Chat.gRPC.Tests/Interceptors/ExceptionHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Core.Testing;
+using Grpc.Core;
+using Chat.gRPC.Interceptors;
+using Serilog;
+using FluentAssertions;
+
+namespace Chat.gRPC.Tests.Interceptors
+{
+    public class ExceptionHelperTests
+    {
+        private readonly ServerCallContext _serverCallContext;
+        private readonly ILogger _logger;
+
+        public ExceptionHelperTests()
+        {
+            _serverCallContext = TestServerCallContext.Create(String.Empty, String.Empty, DateTime.Now,
+            new Metadata(), new CancellationTokenSource().Token, String.Empty, null,
+            null, null, null, null);
+
+            _logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.TestCorrelator().CreateLogger();
+        }
+
+        [Fact]
+        public void ExceptionHelper_TimeOutException_ShouldBeHandled()
+        {
+            //Arrange
+            var exception = new TimeoutException();
+            var correlationId = Guid.NewGuid();
+
+            //Act
+            var rpcException = exception.Handle(_serverCallContext, _logger, correlationId);
+
+            //Assert
+            rpcException.Status.StatusCode.Should().Be(StatusCode.Internal);
+        }
+
+        [Fact]
+        public void ExceptionHelper_RPCException_ShouldBeHandled()
+        {
+            //Arrange
+            var exception = new RpcException(Status.DefaultCancelled);
+            var correlationId = Guid.NewGuid();
+
+            //Act
+            var rpcException = exception.Handle(_serverCallContext, _logger, correlationId);
+
+            //Assert
+            rpcException.Status.StatusCode.Should().Be(StatusCode.Cancelled);
+            rpcException.Trailers.Should().Contain(t => t.Value.Equals(correlationId.ToString()));
+        }
+
+
+        [Fact]
+        public void ExceptionHelper_AnyKindOfException_ShouldBeHandled()
+        {
+            //Arrange
+            var exception = new ArgumentNullException();
+            var correlationId = Guid.NewGuid();
+
+            //Act
+            var rpcException = exception.Handle(_serverCallContext, _logger, correlationId);
+
+            //Assert
+            rpcException.Status.StatusCode.Should().Be(StatusCode.Internal);
+            rpcException.Trailers.Should().Contain(t => t.Value.Equals(correlationId.ToString()));
+        }
+
+    }
+}
---
+++ src/Chat.gRPC.Tests/Interceptors/ExceptionInterceptorTests.cs
@@ -0,0 +1,89 @@
+﻿using Chat.gRPC.Interceptors;
+using Grpc.Core;
+using Serilog;
+
+namespace Chat.gRPC.Tests.Interceptors
+{
+    public class ExceptionInterceptorTests
+    {
+        private readonly ILogger _logger;
+        private readonly Mock<ServerCallContext> _context;
+        private readonly ExceptionInterceptor _interceptor;
+
+        public ExceptionInterceptorTests()
+        {
+            _logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.TestCorrelator().CreateLogger();
+            _context = new Mock<ServerCallContext>();
+            _interceptor = new ExceptionInterceptor(_logger);
+        }
+
+        [Fact]
+        public async Task ExceptionInterceptor_ShouldInterceptUnaryServerMethod()
+        {
+            //Arrange
+            var serverMethod = new Mock<UnaryServerMethod<string, string>>();
+            serverMethod.Setup(s => s.Invoke(It.IsAny<string>(), _context.Object)).Throws(new TimeoutException());
+
+            //Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await _interceptor.UnaryServerHandler(It.IsAny<string>(), _context.Object, serverMethod.Object);
+            });
+
+            //Assert
+            exception.Should().BeAssignableTo<RpcException>();
+        }
+
+        [Fact]
+        public async Task ExceptionInterceptor_ShouldInterceptClientStreamingServerMethod()
+        {
+            //Arrange
+            var serverMethod = new Mock<ClientStreamingServerMethod<string, string>>();
+            serverMethod.Setup(s => s.Invoke(It.IsAny<IAsyncStreamReader<string>>(), _context.Object)).Throws(new TimeoutException());
+
+            //Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await _interceptor.ClientStreamingServerHandler(It.IsAny<IAsyncStreamReader<string>>(), _context.Object, serverMethod.Object);
+            });
+
+            //Assert
+            exception.Should().BeAssignableTo<RpcException>();
+        }
+
+
+        [Fact]
+        public async Task ExceptionInterceptor_ShouldInterceptServerStreamingServerMethod()
+        {
+            //Arrange
+            var serverMethod = new Mock<ServerStreamingServerMethod<string, string>>();
+            serverMethod.Setup(s => s.Invoke(It.IsAny<string>(), It.IsAny<IServerStreamWriter<string>>(), _context.Object)).Throws(new TimeoutException());
+
+            //Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await _interceptor.ServerStreamingServerHandler(It.IsAny<string>(), It.IsAny<IServerStreamWriter<string>>(), _context.Object, serverMethod.Object);
+            });
+
+            //Assert
+            exception.Should().BeAssignableTo<RpcException>();
+        }
+
+        [Fact]
+        public async Task ExceptionInterceptor_ShouldInterceptDuplexStreamingServerMethod()
+        {
+            //Arrange
+            var serverMethod = new Mock<DuplexStreamingServerMethod<string, string>>();
+            serverMethod.Setup(s => s.Invoke(It.IsAny<IAsyncStreamReader<string>>(), It.IsAny<IServerStreamWriter<string>>(), _context.Object)).Throws(new TimeoutException());
+
+            //Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await _interceptor.DuplexStreamingServerHandler(It.IsAny<IAsyncStreamReader<string>>(), It.IsAny<IServerStreamWriter<string>>(), _context.Object, serverMethod.Object);
+            });
+
+            //Assert
+            exception.Should().BeAssignableTo<RpcException>();
+        }
+    }
+}
